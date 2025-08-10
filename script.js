e3607130f924251206e71ca1a8ce44982d3bbcb8
Metanoia3 <138525769+Metanoia3@users.noreply.github.com>
2025-08-10 19:19:16 -0400

    testing: add CSS & JS and link them

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿console.log('Kasil: testing branch JS loaded');
+document.addEventListener('DOMContentLoaded', () => {
+  const h1 = document.querySelector('h1');
+  if (h1) h1.insertAdjacentHTML('afterend', '<p id=\"js-ok\">JS is working ✅</p>');
+});
